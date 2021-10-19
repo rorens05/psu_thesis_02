2642538d58ff8da8a993617a2067455e271ec1f3
Laurence Bautista <rorens05@gmail.com>
2021-10-19 22:54:11 +0800

    Remove unnecessary files

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
-
   def index;
-  # redirect_to admin_dashboard_path
+    redirect_to admin_dashboard_path
   end
-
 end
